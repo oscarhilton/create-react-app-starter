1b6464e8e3018c7b676ad9c3543c099ee8783cdb
Oscar Hilton <oscarhilton@hotmail.com>
2023-10-17 23:58:03 +0100

    MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [word, setWord] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`https://word2vec.objobj.xyz/api/similar/${word}`);
+    const data = await response.json();
+    setResult(data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            value={word} 
+            onChange={(e) => setWord(e.target.value)} 
+            placeholder="Type a word" 
+          />
+          <button type="submit">Submit</button>
+        </form>
+        {result && (
+          <div className="Result">
+            <h3>Similar words for {word}:</h3>
+            <pre>{result && result.similar.map(item => (
+              <div>{item}</div>
+            ))}</pre>
+          </div>
+        )}
       </header>
     </div>
   );
